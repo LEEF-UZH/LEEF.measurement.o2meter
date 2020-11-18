92d6a6fc0d38506636f36fd12f00f32f6fefbb21
Rainer M. Krug <Rainer@krugs.de>
2020-11-18 15:55:48 +0100

    Update extractor_respirometer.R

--- R/extractor_respirometer.R
+++ R/extractor_respirometer.R
@@ -23,78 +23,42 @@ extractor_o2meter <- function(
   # Get csv file names ------------------------------------------------------
 
   o2meter_path <- file.path( input, "o2meter" )
-  o2meter_files <- list.files(
+  fn <- list.files(
     path = o2meter_path,
     pattern = "*.csv",
     full.names = TRUE,
     recursive = TRUE
   )
 
-  if (length(o2meter_files) == 0) {
+  if (length(fn) == 0) {
     message("nothing to extract\n")
     message("\n########################################################\n")
     return(invisible(FALSE))
   }
 
+  if (length(fn) > 1) {
+    message("Only one file expected - aborting\n")
+    message("\n########################################################\n")
+    return(invisible(FALSE))
+  }
+
   # Read file ---------------------------------------------------------------
-  colNames <- c(
-    "date", "time", "user",
-    "sensorid", "sensorname",
-    "delta_t", "unit_delta_t",
-    "value", "unit_value",
-    "mode", "phase", "unit_phase",
-    "amplitude", "unit_amplitude",
-    "temp", "unit_temp",
-    "patm", "unit_patm",
-    "salinity", "unit_salinity",
-    "error",
-    "cal0", "unit_cal0",
-    "t0", "unit_t0",
-    "o2_2nd", "unit_o2_2nd",
-    "cal2nd", "unit_cal2nd",
-    "t2nd", "unit_t2nd",
-    "calpressure", "unit_calpressure",
-    "f1", "dphi1", "dksv1",
-    "dphi2", "dksv2", "m",
-    "cal_mode", "signalledcurrent",
-    "referenceledcurrent", "referenceamplitude",
-    "deviceserial", "fwversion",
-    "sensortype", "batchid",
-    "calibration_date", "sensor_lot", "presens_calibrated_sensor",
-    "battery_voltage", "unit_battery_voltage",
-    "empty"
+
+  dat <- read.table(
+    fn,
+    skip = 1,
+    fill = TRUE
   )
-  res <- lapply(
-    o2meter_files,
-    function(fn) {
-      x <- read.delim(
-        file = fn,
-        sep = ";",
-        skip = 2,
-        header = FALSE,
-        fill = TRUE,
-        stringsAsFactors = FALSE,
-        strip.white = TRUE,
-        fileEncoding = "ISO-8859-1",
-        col.names = colNames
-      ) [-length(colNames)]
-      f <- file(fn)
-      attr(x, "PresensVersion") <- readLines(f, n = 1)
-      close(f)
-      return(x)
-    }
-  )
-  # combine intu one large tibble
-  res <- do.call(rbind, res)  ## dplyr::bind_rows(res)
-  ## res <- dplyr::filter(res, date != "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+  names(dat) <- dat[1,]
+  dat <- dat[c(-1, -nrow(dat)),]
 
   # SAVE --------------------------------------------------------------------
 
   add_path <- file.path( output, "o2meter" )
   dir.create( add_path, recursive = TRUE, showWarnings = FALSE )
-  names(res) <- tolower(names(res))
+  names(dat) <- tolower(names(dat))
   saveRDS(
-    object = res,
+    object = dat,
     file = file.path(add_path, "o2meter.rds")
   )
 
